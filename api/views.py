28cd9884a449e0f93937ce812ef2c5088ce6b7dd
Joseph Tang <josephtang765@gmail.com>
2017-10-03 23:00:31 -0700

    Implement create method on TodoViewSet

--- api/views.py
+++ api/views.py
@@ -3,6 +3,7 @@ from todo.models import Todo
 from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import detail_route, list_route
 from serializers import TodoSerializer
 
 
@@ -18,3 +19,11 @@ class TodoViewSet(viewsets.ViewSet):
         queryset = Todo.objects.filter(owner=request.user).order_by('-due_date')
         serializer = TodoSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    # @detail_route(methods=['post'])
+    def create(self, request):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.create(serializer.validated_data)
+        return Response({'status': 'Todo created'})
+
