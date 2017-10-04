7433f97f808235daf23b6d27de99a7edc30c1c0c
Joseph Tang <josephtang765@gmail.com>
2017-10-03 23:09:47 -0700

    Allow null values on blank fields

--- todo/models.py
+++ todo/models.py
@@ -16,12 +16,12 @@ class TodoList(models.Model):
 class Todo(models.Model):
     owner = models.ForeignKey(User)
     title = models.CharField(max_length=256)
-    description = models.CharField(max_length=10000, blank=True)
-    priority = models.IntegerField(blank=True)
+    description = models.CharField(max_length=10000, blank=True, null=True)
+    priority = models.IntegerField(blank=True, null=True)
     todo_list = models.ForeignKey(TodoList)
-    due_date = models.DateTimeField('due date', blank=True)
-    start_date = models.DateTimeField('start date', blank=True)
-    end_date = models.DateTimeField('end date', blank=True)
+    due_date = models.DateTimeField('due date', blank=True, null=True)
+    start_date = models.DateTimeField('start date', blank=True, null=True)
+    end_date = models.DateTimeField('end date', blank=True, null=True)
 
     def __unicode__(self):
         return self.title
