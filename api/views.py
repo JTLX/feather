3ac8a36af1c29c0263263199ef43e51805c44216
Joseph Tang <josephtang765@gmail.com>
2017-10-03 22:03:25 -0700

    Show users only their todos

--- api/views.py
+++ api/views.py
@@ -1,14 +1,20 @@
 from django.contrib.auth.models import User, Group
 from todo.models import Todo
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from serializers import TodoSerializer
 
-import sys
 
-print (sys.path)
-class TodoViewSet(viewsets.ModelViewSet):
+class TodoViewSet(viewsets.ViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = Todo.objects.all().order_by('-due_date')
+    queryset = Todo.objects.all()
     serializer_class = TodoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def list(self, request):
+        queryset = Todo.objects.filter(owner=request.user).order_by('-due_date')
+        serializer = TodoSerializer(queryset, many=True)
+        return Response(serializer.data)
